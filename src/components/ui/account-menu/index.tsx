03b92f97530fe6bf7bdf7582ea7d9d6645a5ba5f
Ronaldo <ronaldomjunior05@gmail.com>
2024-06-09 21:27:08 -0300

    feat: add sign out functionality

--- src/components/ui/account-menu/index.tsx
+++ src/components/ui/account-menu/index.tsx
@@ -1,9 +1,11 @@
-import { useQuery } from '@tanstack/react-query'
+import { useMutation, useQuery } from '@tanstack/react-query'
 import { Building, ChevronDown, LogOut } from 'lucide-react'
 import { useState } from 'react'
+import { useNavigate } from 'react-router-dom'
 
 import { getManagedRestaurant } from '@/api/get-managed-restaurant'
 import { getProfile } from '@/api/get-profile'
+import { signOut } from '@/api/sign-out'
 
 import { Button } from '../button'
 import { Dialog, DialogTrigger } from '../dialog'
@@ -21,6 +23,8 @@ import { StoreProfileDialog } from '../store-profile-dialog'
 export function AccountMenu() {
   const [isProfileDialogOpen, setIsProfileDialogOpen] = useState(false)
 
+  const navigate = useNavigate()
+
   const { data: profile, isLoading: isProfileLoading } = useQuery({
     queryKey: ['profile'],
     queryFn: getProfile,
@@ -32,6 +36,13 @@ export function AccountMenu() {
       queryFn: getManagedRestaurant,
       staleTime: Infinity,
     })
+
+  const { mutateAsync: signOutFn, isPending: isSigningOut } = useMutation({
+    mutationFn: signOut,
+    onSuccess: () => {
+      navigate('/sign-in', { replace: true })
+    },
+  })
 
   const closeProfileDialog = () => setIsProfileDialogOpen(false)
 
@@ -74,9 +85,15 @@ export function AccountMenu() {
               <span>Perfil da loja</span>
             </DropdownMenuItem>
           </DialogTrigger>
-          <DropdownMenuItem className="text-rose-500 dark:text-rose-400">
-            <LogOut className="mr-2 size-4" />
-            <span>Sair</span>
+          <DropdownMenuItem
+            asChild
+            disabled={isSigningOut}
+            className="text-rose-500 dark:text-rose-400"
+          >
+            <button className="w-full" onClick={() => signOutFn()}>
+              <LogOut className="mr-2 h-4 w-4" />
+              <span>Sair</span>
+            </button>
           </DropdownMenuItem>
         </DropdownMenuContent>
       </DropdownMenu>
